a809d58c82bb47a66da2d80f0166fb4bfed9ab8d
Bas Cantrijn <bas-can@hotmail.com>
2018-05-17 23:04:58 +0200

    Add project files.

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+	public GameObject explosionPrefab;
+	public float detonationTime = 3;
+	public float detonationRange = 2;
+
+	private bool isExploded = false;
+	private Rigidbody2D rigid;
+
+
+
+	/// <summary>
+	/// When it spawns, start a timer to detonate it.
+	/// </summary>
+	void Awake ()
+	{
+		rigid = GetComponent<Rigidbody2D>();
+
+		Invoke("Detonate", detonationTime);
+	}
+
+
+	/// <summary>
+	/// When the bomb is dropped, it has no collision. But once the player leaves its trigger, 
+	/// collision is turned on. (So you can block people!)
+	/// </summary>
+	void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+			Collider2D collider = GetComponent<Collider2D>();
+			collider.isTrigger = false;
+		}
+	}
+
+
+	/// <summary>
+	/// Public function to detonate this bomb.
+	/// </summary>
+	public void Detonate()
+	{
+		if (isExploded)
+			return;
+
+		isExploded = true;
+
+		// Change the bomb sprite to an explosion and disable and collider (for raycast purposes)
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		renderer.enabled = false;
+
+		Collider2D collider = GetComponent<Collider2D>();
+		collider.enabled = false;
+
+
+		// Destroy everything in all directions
+		Instantiate(explosionPrefab, transform.position, Quaternion.identity, this.transform);
+
+		DetonateDirection(Vector2.left, detonationRange);
+		DetonateDirection(Vector2.up, detonationRange);
+		DetonateDirection(Vector2.right, detonationRange);
+		DetonateDirection(Vector2.down, detonationRange);
+	}
+
+
+	/// <summary>
+	/// Internal function to destroy closest object in that direction. Direction should always be 
+	/// 1 meter on that x or y axis. (Ex. Vector2.up = [x: 0, y: 1], Vector2.left = [x: -1, y: 0])
+	/// </summary>
+	private void DetonateDirection(Vector2 direction, float distance)
+	{
+		RaycastHit2D hit = Physics2D.Raycast(rigid.position, direction, distance);
+		float explosionDistance = distance;
+
+		if (hit.transform != null)
+		{
+			explosionDistance = (hit.distance + 1);
+			Debug.Log("hit tag " + hit.transform.tag);
+			switch (hit.transform.tag)
+			{
+				// Kill a player when hitting it
+				case "Player":
+					Player player = hit.transform.GetComponent<Player>();
+					if (player != null)
+					{
+						player.Kill();
+					}
+					break;
+				
+				// Destroy a box when hitting it
+				case "Box":
+					Destroy(hit.transform.gameObject);
+					break;
+
+				// Detonate another bomb when hitting it
+				case "Bomb":
+					Bomb bomb = hit.transform.GetComponent<Bomb>();
+					if (bomb != null)
+					{
+						bomb.Detonate();
+					}
+					break;
+				
+				// Everything else it hit, but isn't explodable.
+				default:
+					explosionDistance = hit.distance;
+					break;
+			}
+		}
+
+		// Spawn fire particles in that direction
+		Debug.Log("hit distance " + explosionDistance + " direction: " + direction);
+		for (int step = 1; step <= explosionDistance; step++)
+		{
+			Vector3 offsetPosition = new Vector3(direction.x, direction.y, 0) * step;
+			Instantiate(explosionPrefab, transform.position + offsetPosition, Quaternion.identity, this.transform);
+		}
+
+		// Destroy the bomb object and all effects
+		Destroy(gameObject, 1f);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public string horizontalInput;
+	public string verticalInput;
+	public KeyCode bombKey;
+	public float walkSpeed;
+
+	public GameObject bombPrefab;
+	public Sprite skeletonSprite;
+
+
+	private Rigidbody2D rigid;
+	private bool isDead = false;
+	
+
+	/// <summary>
+	/// Important! Retrieve this object its own components in Awake, and other objects their 
+	/// components in Start()!
+	/// </summary>
+	void Awake()
+	{
+		rigid = GetComponent<Rigidbody2D>();
+	}
+
+
+	/// <summary>
+	/// Movement and bomb dropping code for this player.
+	/// </summary>
+	void FixedUpdate ()
+	{
+		if (isDead)
+			return;
+
+		float horizontalMovement = Input.GetAxis(horizontalInput);
+		float verticalMovement = Input.GetAxis(verticalInput);
+		
+		Vector2 movement = new Vector2(horizontalMovement, verticalMovement) * walkSpeed * Time.fixedDeltaTime;
+		rigid.MovePosition(rigid.position + movement);
+
+		// Dropping the bomb
+		if (Input.GetKeyDown(bombKey))
+		{
+			// Align with grid
+			float x = Mathf.FloorToInt(rigid.position.x) + 0.5f;
+			float y = Mathf.FloorToInt(rigid.position.y) + 0.5f;
+			Vector3 bombPos = new Vector3(x, y, 0);
+
+			Instantiate(bombPrefab, bombPos, Quaternion.identity);
+		}
+	}
+
+
+	/// <summary>
+	/// Public method to kill this player.
+	/// </summary>
+	public void Kill()
+	{
+		if (isDead)
+			return;
+
+		isDead = true;
+
+		SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+		renderer.sprite = skeletonSprite;
+	}
+}
